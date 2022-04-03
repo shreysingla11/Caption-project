51517c0333d128106c398e397e9765c8057dcfc9
Bhavnoor Singh Marok <bhavnoor456@gmail.com>
2022-04-03 15:11:57 +0530

    updated

--- glo.py
+++ glo.py
@@ -16,6 +16,12 @@ from torchvision.utils import make_grid
 
 
 
+
+
+
+
+
+
 def build_gauss_kernel(size=5, sigma=1.0, n_channels=1, cuda=False):
     if size % 2 != 1:
         raise ValueError("kernel size must be uneven")
@@ -89,26 +95,6 @@ class IndexedDataset(Dataset):
         return (img, label, idx)
 
 
-class Generator(nn.Module):
-    def __init__(self, code_dim, n_filter=64, out_channels=3):
-        super(Generator, self).__init__()
-        self.code_dim = code_dim
-        nf = n_filter
-        self.dcnn = nn.Sequential(
-            nn.ConvTranspose2d(code_dim, nf * 8, 4, 1, 0, bias=False), # 2x2
-            nn.BatchNorm2d(nf * 8), nn.ReLU(True),
-            nn.ConvTranspose2d(nf * 8, nf * 4, 4, 2, 1, bias=False), # 4x4
-            nn.BatchNorm2d(nf * 4), nn.ReLU(True),
-            nn.ConvTranspose2d(nf * 4, nf * 2, 4, 2, 1, bias=False), # 8x8
-            nn.BatchNorm2d(nf * 2), nn.ReLU(True),
-            nn.ConvTranspose2d(nf * 2, nf    , 4, 2, 1, bias=False), # 16x16
-            nn.BatchNorm2d(nf), nn.ReLU(True),
-            nn.ConvTranspose2d(nf, out_channels, 4, 2, 1, bias=False), # 32x32
-            nn.Tanh(),
-        )
-
-    def forward(self, code):
-        return self.dcnn(code.view(code.size(0), self.code_dim, 1, 1))
 
 
 def project_l2_ball(z):
--- main.py
+++ main.py
@@ -16,6 +16,20 @@ from torchvision.utils import make_grid
 
 
 
+
+
+emb_dim = 512  # dimension of word embeddings
+attention_dim = 512  # dimension of attention linear layers
+decoder_dim = 512  # dimension of decoder RNN
+dropout = 0.5
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # sets device for model and PyTorch tensors
+cudnn.benchmark = True  # set to true only if inputs to model are fixed size; otherwise lot of computational overhead
+
+
+
+
+
+
 def build_gauss_kernel(size=5, sigma=1.0, n_channels=1, cuda=False):
     if size % 2 != 1:
         raise ValueError("kernel size must be uneven")
@@ -89,6 +103,193 @@ class IndexedDataset(Dataset):
         return (img, label, idx)
 
 
+
+
+
+
+
+
+
+
+
+
+
+class Attention(nn.Module):
+    """
+    Attention Network.
+    """
+
+    def __init__(self, encoder_dim, decoder_dim, attention_dim):
+        """
+        :param encoder_dim: feature size of encoded images
+        :param decoder_dim: size of decoder's RNN
+        :param attention_dim: size of the attention network
+        """
+        super(Attention, self).__init__()
+        self.encoder_att = nn.Linear(encoder_dim, attention_dim)  # linear layer to transform encoded image
+        self.decoder_att = nn.Linear(decoder_dim, attention_dim)  # linear layer to transform decoder's output
+        self.full_att = nn.Linear(attention_dim, 1)  # linear layer to calculate values to be softmax-ed
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)  # softmax layer to calculate weights
+
+    def forward(self, encoder_out, decoder_hidden):
+        """
+        Forward propagation.
+
+        :param encoder_out: encoded images, a tensor of dimension (batch_size, num_pixels, encoder_dim)
+        :param decoder_hidden: previous decoder output, a tensor of dimension (batch_size, decoder_dim)
+        :return: attention weighted encoding, weights
+        """
+        att1 = self.encoder_att(encoder_out)  # (batch_size, num_pixels, attention_dim)
+        att2 = self.decoder_att(decoder_hidden)  # (batch_size, attention_dim)
+        att = self.full_att(self.relu(att1 + att2.unsqueeze(1))).squeeze(2)  # (batch_size, num_pixels)
+        alpha = self.softmax(att)  # (batch_size, num_pixels)
+        attention_weighted_encoding = (encoder_out * alpha.unsqueeze(2)).sum(dim=1)  # (batch_size, encoder_dim)
+
+        return attention_weighted_encoding, alpha
+
+
+class Generator(nn.Module):
+    """
+    Decoder.
+    """
+
+    def __init__(self, attention_dim, embed_dim, decoder_dim, vocab_size, encoder_dim=2048, dropout=0.5):
+        """
+        :param attention_dim: size of attention network
+        :param embed_dim: embedding size
+        :param decoder_dim: size of decoder's RNN
+        :param vocab_size: size of vocabulary
+        :param encoder_dim: feature size of encoded images
+        :param dropout: dropout
+        """
+        super(Generator, self).__init__()
+
+        self.encoder_dim = encoder_dim
+        self.attention_dim = attention_dim
+        self.embed_dim = embed_dim
+        self.decoder_dim = decoder_dim
+        self.vocab_size = vocab_size
+        self.dropout = dropout
+
+        self.attention = Attention(encoder_dim, decoder_dim, attention_dim)  # attention network
+
+        self.embedding = nn.Embedding(vocab_size, embed_dim)  # embedding layer
+        self.dropout = nn.Dropout(p=self.dropout)
+        self.decode_step = nn.LSTMCell(embed_dim + encoder_dim, decoder_dim, bias=True)  # decoding LSTMCell
+        self.init_h = nn.Linear(encoder_dim, decoder_dim)  # linear layer to find initial hidden state of LSTMCell
+        self.init_c = nn.Linear(encoder_dim, decoder_dim)  # linear layer to find initial cell state of LSTMCell
+        self.f_beta = nn.Linear(decoder_dim, encoder_dim)  # linear layer to create a sigmoid-activated gate
+        self.sigmoid = nn.Sigmoid()
+        self.fc = nn.Linear(decoder_dim, vocab_size)  # linear layer to find scores over vocabulary
+        self.init_weights()  # initialize some layers with the uniform distribution
+
+    def init_weights(self):
+        """
+        Initializes some parameters with values from the uniform distribution, for easier convergence.
+        """
+        self.embedding.weight.data.uniform_(-0.1, 0.1)
+        self.fc.bias.data.fill_(0)
+        self.fc.weight.data.uniform_(-0.1, 0.1)
+
+    def load_pretrained_embeddings(self, embeddings):
+        """
+        Loads embedding layer with pre-trained embeddings.
+
+        :param embeddings: pre-trained embeddings
+        """
+        self.embedding.weight = nn.Parameter(embeddings)
+
+    def fine_tune_embeddings(self, fine_tune=True):
+        """
+        Allow fine-tuning of embedding layer? (Only makes sense to not-allow if using pre-trained embeddings).
+
+        :param fine_tune: Allow?
+        """
+        for p in self.embedding.parameters():
+            p.requires_grad = fine_tune
+
+    def init_hidden_state(self, encoder_out):
+        """
+        Creates the initial hidden and cell states for the decoder's LSTM based on the encoded images.
+
+        :param encoder_out: encoded images, a tensor of dimension (batch_size, num_pixels, encoder_dim)
+        :return: hidden state, cell state
+        """
+        mean_encoder_out = encoder_out.mean(dim=1)
+        h = self.init_h(mean_encoder_out)  # (batch_size, decoder_dim)
+        c = self.init_c(mean_encoder_out)
+        return h, c
+
+    def forward(self, encoder_out, encoded_captions, caption_lengths):
+        """
+        Forward propagation.
+
+        :param encoder_out: encoded images, a tensor of dimension (batch_size, enc_image_size, enc_image_size, encoder_dim)
+        :param encoded_captions: encoded captions, a tensor of dimension (batch_size, max_caption_length)
+        :param caption_lengths: caption lengths, a tensor of dimension (batch_size, 1)
+        :return: scores for vocabulary, sorted encoded captions, decode lengths, weights, sort indices
+        """
+
+        batch_size = encoder_out.size(0)
+        encoder_dim = encoder_out.size(-1)
+        vocab_size = self.vocab_size
+
+        # Flatten image
+        encoder_out = encoder_out.view(batch_size, -1, encoder_dim)  # (batch_size, num_pixels, encoder_dim)
+        num_pixels = encoder_out.size(1)
+
+        # Sort input data by decreasing lengths; why? apparent below
+        caption_lengths, sort_ind = caption_lengths.squeeze(1).sort(dim=0, descending=True)
+        encoder_out = encoder_out[sort_ind]
+        encoded_captions = encoded_captions[sort_ind]
+
+        # Embedding
+        embeddings = self.embedding(encoded_captions)  # (batch_size, max_caption_length, embed_dim)
+
+        # Initialize LSTM state
+        h, c = self.init_hidden_state(encoder_out)  # (batch_size, decoder_dim)
+
+        # We won't decode at the <end> position, since we've finished generating as soon as we generate <end>
+        # So, decoding lengths are actual lengths - 1
+        decode_lengths = (caption_lengths - 1).tolist()
+
+        # Create tensors to hold word predicion scores and alphas
+        predictions = torch.zeros(batch_size, max(decode_lengths), vocab_size).to(device)
+        alphas = torch.zeros(batch_size, max(decode_lengths), num_pixels).to(device)
+
+        # At each time-step, decode by
+        # attention-weighing the encoder's output based on the decoder's previous hidden state output
+        # then generate a new word in the decoder with the previous word and the attention weighted encoding
+        for t in range(max(decode_lengths)):
+            batch_size_t = sum([l > t for l in decode_lengths])
+            attention_weighted_encoding, alpha = self.attention(encoder_out[:batch_size_t],
+                                                                h[:batch_size_t])
+            gate = self.sigmoid(self.f_beta(h[:batch_size_t]))  # gating scalar, (batch_size_t, encoder_dim)
+            attention_weighted_encoding = gate * attention_weighted_encoding
+            h, c = self.decode_step(
+                torch.cat([embeddings[:batch_size_t, t, :], attention_weighted_encoding], dim=1),
+                (h[:batch_size_t], c[:batch_size_t]))  # (batch_size_t, decoder_dim)
+            preds = self.fc(self.dropout(h))  # (batch_size_t, vocab_size)
+            predictions[:batch_size_t, t, :] = preds
+            alphas[:batch_size_t, t, :] = alpha
+
+        return predictions, encoded_captions, decode_lengths, alphas, sort_ind
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def project_l2_ball(z):
     """ project the vectors in z onto the l2 unit norm ball"""
     return z / np.maximum(np.sqrt(np.sum(z**2, axis=1))[:, np.newaxis], 1)
@@ -104,7 +305,7 @@ def main(
         image_output_prefix: ('Prefix for image output', 
                               'option', 'o')='glo',
         code_dim: ('Dimensionality of latent representation space', 
-                   'option', 'd', int)=128, 
+                   'option', 'd', int)=2048, 
         epochs: ('Number of epochs to train', 
                  'option', 'e', int)=25,
         use_cuda: ('Use GPU?', 
@@ -112,9 +313,9 @@ def main(
         batch_size: ('Batch size', 
                      'option', 'b', int)=128,
         lr_g: ('Learning rate for generator', 
-               'option', None, float)=1.,
+               'option', None, float)=4e-1,
         lr_z: ('Learning rate for representation_space', 
-               'option', None, float)=10.,
+               'option', None, float)=1.,
         max_num_samples: ('Cap on the number of samples from the LSUN dataset', 
                           'option', 'n', int)=-1,
         init: ('Initialization strategy for latent represetation vectors', 
@@ -128,23 +329,34 @@ def main(
     def maybe_cuda(tensor):
         return tensor.cuda() if use_cuda else tensor
 
-    train_set = IndexedDataset(
-        LSUN(lsun_data_dir, classes=['church_outdoor_train'], 
-             transform=transforms.Compose([
-                 transforms.Resize(64),
-                 transforms.CenterCrop(64),
-                 transforms.ToTensor(),
-                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-             ]))
-    )
-    train_loader = torch.utils.data.DataLoader(
-        train_set, batch_size=batch_size, 
-        shuffle=True, drop_last=True,
-        num_workers=8, pin_memory=use_cuda,
-    )
-    # we don't really have a validation set here, but for visualization let us 
-    # just take the first couple images from the dataset
-    val_loader = torch.utils.data.DataLoader(train_set, shuffle=False, batch_size=8*8)
+    # train_set = IndexedDataset(
+    #     LSUN(lsun_data_dir, classes=['church_outdoor_train'], 
+    #          transform=transforms.Compose([
+    #              transforms.Resize(64),
+    #              transforms.CenterCrop(64),
+    #              transforms.ToTensor(),
+    #              transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    #          ]))
+    # )
+    # train_loader = torch.utils.data.DataLoader(
+    #     train_set, batch_size=batch_size, 
+    #     shuffle=True, drop_last=True,
+    #     num_workers=8, pin_memory=use_cuda,
+    # )
+
+
+    train_set = CaptionDataset(data_folder, data_name, 'TRAIN', transform=transforms.Compose([normalize]))
+
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
+    train_loader = torch.utils.data.DataLoader(train_set,batch_size=batch_size, shuffle=True, num_workers=workers, pin_memory=True)
+
+
+
+    # val_loader = torch.utils.data.DataLoader(train_set, shuffle=False, batch_size=8*8)
+    val_set = CaptionDataset(data_folder, data_name, 'VAL', transform=transforms.Compose([normalize]))
+    val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size, shuffle=True, num_workers=workers, pin_memory=True)
+
+
 
     if max_num_samples > 0:
         train_set.base.length = max_num_samples
@@ -178,8 +390,19 @@ def main(
 
     Z = project_l2_ball(Z)
 
-    g = maybe_cuda(Generator(code_dim))
-    loss_fn = LapLoss(max_levels=3) if loss == 'lap_l1' else nn.MSELoss()
+    global emb_dim, attention_dim, decoder_dim, dropout
+
+    word_map_file = os.path.join(data_folder, 'WORDMAP_' + data_name + '.json')
+    with open(word_map_file, 'r') as j:
+        word_map = json.load(j)
+
+
+
+    g = maybe_cuda(Generator(attention_dim,emb_dim,decoder_dim,len(word_map),code_dim,dropout))
+    #loss_fn = LapLoss(max_levels=3) if loss == 'lap_l1' else nn.MSELoss()
+    
+    loss_fn = nn.CrossEntropyLoss().to(device)
+    
     zi = maybe_cuda(torch.zeros((batch_size, code_dim)))
     zi = Variable(zi, requires_grad=True)
     optimizer = SGD([
@@ -195,12 +418,12 @@ def main(
         losses = []
         progress = tqdm(total=len(train_loader), desc='epoch % 3d' % epoch)
 
-        for i, (Xi, yi, idx) in enumerate(train_loader):
+        for i, (Xi, caps, caplens) in enumerate(train_loader):
             Xi = Variable(maybe_cuda(Xi))
             zi.data = maybe_cuda(torch.FloatTensor(Z[idx.numpy()]))
 
             optimizer.zero_grad()
-            rec = g(zi)
+            rec = g(zi,caps,caplens)
             loss = loss_fn(rec, Xi)
             loss.backward()
             optimizer.step()
